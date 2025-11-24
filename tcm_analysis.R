bda84d89173f9270c5bd040fe285e61fadc9817a
Hinna <hinna@i.smu.edu.cn>
2025-11-24 19:55:50 +0800

    update

--- tcm_analysis.R
+++ tcm_analysis.R
@@ -178,7 +178,7 @@ p5 <- ggraph(ppi_new1, layout = 'kk') +
       data_id = name
     )
   ) +
-  geom_text_repel(data = td_filter(degree > 60),
+  geom_text_repel(data = td_filter(degree > 20),
                   mapping = aes(x = x, y = y, label = name), bg.color ='white'
   ) +
   theme_graph()
@@ -186,6 +186,120 @@ p5 <- ggraph(ppi_new1, layout = 'kk') +
 girafe(ggobj = p5)
 
 
+## add score to each cluster
+louvain_ranking <- score_graph_clusters(ppi_new1, cluster_attr = "louvain_cluster")
+print(louvain_ranking)  ## cluster 2 was selected for further analysis
+
+# select cluster 2 nodes
+c2_nodes <- V(ppi_new)$name[V(ppi_new1)$louvain_cluster == "2"]
+subg_cluster2 <- induced_subgraph(ppi_new1, vids = c2_nodes)
+
+# cluster 2 enrichment analysis
+c2 <- enrichGO(c2_nodes, OrgDb='org.Hs.eg.db', keyType="SYMBOL", ont = "all")
+c2go <- gglollipop(c2, line.col = "orange", split = "ONTOLOGY",
+                    line.type = "dashed", palette = "PiYG", top_n = 5, show_count = F) + 
+  facet_grid(ONTOLOGY ~ ., scales = "free_y") +
+  theme(strip.text.y = element_text(angle = 0)) 
+c2go
+
+# select cluster 3 nodes
+c3_nodes <- V(ppi_new)$name[V(ppi_new1)$louvain_cluster == "3"]
+subg_cluster3 <- induced_subgraph(ppi_new1, vids = c3_nodes)
+
+# cluster 3 enrichment analysis
+c3 <- enrichGO(c3_nodes, OrgDb='org.Hs.eg.db', keyType="SYMBOL", ont = "all")
+c3go <- gglollipop(c3, line.col = "orange", split = "ONTOLOGY",
+                   line.type = "dashed", palette = "PiYG", top_n = 5, show_count = F) + 
+  facet_grid(ONTOLOGY ~ ., scales = "free_y") +
+  theme(strip.text.y = element_text(angle = 0)) 
+c3go
+
+
+
+ggtangle::ggplot(subg_cluster2, layout = "kk") +
+  geom_edge(
+    colour  = "grey70",
+    alpha   = 0.75,
+    linewidth = 0.15,
+    lineend  = "round"
+  ) +
+  geom_point(
+    aes(colour = Score_network, size = betweenness),
+    alpha = 0.9
+  ) +
+  geom_text(
+    aes(label = name),
+    colour      = "black",    
+    size        = 3,        
+    fontface    = "bold",
+    show.legend = FALSE      
+  )+
+  scale_color_gradientn(
+    name    = "node score",
+    colours = c("#4DBBD5FF", "#FFDC91", "#E64B35FF"),
+    limits  = range(igraph::vertex_attr(subg)$Score_network)
+  ) +
+  scale_size_continuous(
+    name  = "Betweenness",
+    range = c(10, 15)   
+  ) +
+  theme_void() +
+  theme(
+    legend.position = "right",
+    legend.title    = element_text(size = 11, face = "bold"),
+    legend.text     = element_text(size = 9),
+    plot.margin     = unit(c(0.6, 0.8, 0.6, 0.8), "cm"),
+    plot.background = element_rect(fill = "white", colour = NA),
+    text            = element_text(face = "bold")
+  )
+
+
+ggtangle::ggplot(subg_cluster3, layout = "kk") +
+  geom_edge(
+    colour  = "grey70",
+    alpha   = 0.75,
+    linewidth = 0.15,
+    lineend  = "round"
+  ) +
+  geom_point(
+    aes(colour = Score_network, size = betweenness),
+    alpha = 0.9
+  ) +
+  geom_text(
+    aes(label = name),
+    colour      = "black",    
+    size        = 3,        
+    fontface    = "bold",
+    show.legend = FALSE      
+  )+
+  scale_color_gradientn(
+    name    = "node score",
+    colours = c("#4DBBD5FF", "#FFDC91", "#E64B35FF"),
+    limits  = range(igraph::vertex_attr(subg)$Score_network)
+  ) +
+  scale_size_continuous(
+    name  = "Betweenness",
+    range = c(10, 15)   
+  ) +
+  theme_void() +
+  theme(
+    legend.position = "right",
+    legend.title    = element_text(size = 11, face = "bold"),
+    legend.text     = element_text(size = 9),
+    plot.margin     = unit(c(0.6, 0.8, 0.6, 0.8), "cm"),
+    plot.background = element_rect(fill = "white", colour = NA),
+    text            = element_text(face = "bold")
+  )
+
+
+## comparecluster 2 and 3
+library(org.Hs.eg.db)
+library(enrichplot)
+module.genes <- ppi_new1 |> as_tbl_graph() |> activate('nodes') |>tibble::as_tibble() |> filter(louvain_cluster %in% c("2", "3"))
+module.ora.res <- compareCluster(name ~ louvain_cluster, data = module.genes, 
+                                 fun = enrichGO, keyType = "SYMBOL", OrgDb=org.Hs.eg.db, ont = "BP")
+
+dotplot(module.ora.res, label_format = 70)+ set_enrichplot_color(type='fill', transform='log10', colors=c("red","blue"))
 
 
 
@@ -271,7 +385,7 @@ p3 <- ggtangle::ggplot(subg, layout = "kk") +
   ) +
   scale_size_continuous(
     name  = "Betweenness",
-    range = c(10, 20)   
+    range = c(10, 15)   
   ) +
   theme_void() +
   theme(
@@ -287,8 +401,136 @@ print(p3)
 
 
 
+## PPI interaction network
+tkid <- tkplot(subg, canvas.width = 600, canvas.height = 600)
+new_layout <- tkplot.getcoords(tkid)
+
+ggraph(subg, layout = new_layout) + 
+  geom_edge_link(
+    colour    = "grey85",
+    alpha     = 0.6,
+    width     = 0.15,
+    lineend   = "round"
+  ) +
+  
+  geom_node_point(
+    aes(fill = Score_network, size = betweenness), 
+    shape = 21, 
+    colour = "white",  
+    stroke = 0.5,
+    alpha = 1
+  ) +
+  geom_node_text(
+    aes(label = name),
+    colour      = "black",
+    size        = 3.5,
+    fontface    = "bold",
+    show.legend = FALSE
+  ) +
+  scale_fill_gradientn(
+    name    = "Score",
+    colours = c("#4DBBD5FF", "#E64B35FF"),
+    limits  = range(igraph::vertex_attr(subg)$Score_network)
+  ) +
+  scale_size_continuous(
+    name  = "Betweenness",
+    range = c(10, 15),
+    guide = guide_legend(
+      override.aes = list(
+        shape = 21,         
+        colour = "black",   
+        fill = "grey80"     
+      )
+    )
+  ) +
+  coord_fixed() + 
+  theme_void() + 
+  theme(
+    legend.position = "right",
+    legend.title    = element_text(size = 11, face = "bold"),
+    legend.text     = element_text(size = 9),
+    plot.margin     = unit(c(1, 1, 1, 1), "cm"),
+    plot.background = element_rect(fill = "white", colour = NA)
+  )
 
 
+## cluster 2 analysis (immune response)
+# genelist: c2_nodes
+
+# feature selection
+c2_rank <- ppi_res[ppi_res$name %in% c2_nodes, ]
+
+# select top 10
+c2_g <- igraph::induced_subgraph(ppi_new, vids = head(c2_rank$name, 10))
+
+ggraph(c2_g, layout = "kk") + 
+  geom_edge_link(
+    colour    = "grey85",
+    alpha     = 0.6,
+    width     = 0.15,
+    lineend   = "round"
+  ) +
+  
+  geom_node_point(
+    aes(fill = Score_network, size = betweenness), 
+    shape = 21, 
+    colour = "white",  
+    stroke = 0.5,
+    alpha = 1
+  ) +
+  geom_node_text(
+    aes(label = name),
+    colour      = "black",
+    size        = 3.5,
+    fontface    = "bold",
+    show.legend = FALSE
+  ) +
+  scale_fill_gradientn(
+    name    = "Score",
+    colours = c("#4DBBD5FF", "#E64B35FF"),
+    limits  = range(igraph::vertex_attr(subg)$Score_network)
+  ) +
+  scale_size_continuous(
+    name  = "Betweenness",
+    range = c(10, 15),
+    guide = guide_legend(
+      override.aes = list(
+        shape = 21,         
+        colour = "black",   
+        fill = "grey80"     
+      )
+    )
+  ) +
+  coord_fixed() + 
+  theme_void() + 
+  theme(
+    legend.position = "right",
+    legend.title    = element_text(size = 11, face = "bold"),
+    legend.text     = element_text(size = 9),
+    plot.margin     = unit(c(1, 1, 1, 1), "cm"),
+    plot.background = element_rect(fill = "white", colour = NA)
+  )
+
+# get corresponding molecule-target
+qx_c2 <- qx_new[qx_new$target %in% head(c2_rank$name, 10), ]
+qx_ts <- TCM_sankey(qx_c2)
+qx_ts
+
+
+qx_g <- prepare_herb_graph(qx_c2)
+
+qx_p <- ggtangle::ggplot(qx_g, layout = "circle") +  
+  ggtangle::geom_edge(alpha = 0.4) +
+  ggrepel::geom_text_repel(aes(label = name), size = 3.5, max.overlaps = 20) +
+  geom_point(aes(color = type, size = centrality)) +
+  scale_color_manual(values = c(
+    "Herb" = "#E41A1C",      
+    "Molecule" = "#377EB8",    
+    "Target" = "#4DAF4A"       
+  )) +
+  theme_void() 
+
+qx_p
 
 
 
