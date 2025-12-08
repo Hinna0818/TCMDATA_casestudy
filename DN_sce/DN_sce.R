018ec6ed535c3fdea91e8a5b85de4aef3e92d105
Hinna <hinna@i.smu.edu.cn>
2025-12-08 23:57:29 +0800

    add annotation

--- DN_sce/DN_sce.R
+++ DN_sce/DN_sce.R
@@ -2,6 +2,9 @@
 library(Seurat)
 library(ggplot2)
 library(dplyr)
+library(DoubletFinder)
+library(ggsci)
+library(ggsc)
 
 ## preprocess
 # load data and subset
@@ -25,7 +28,6 @@ normalization <- function(srt){
 
 dkd <- normalization(dkd)
 
-
 # PCA
 dkd <- RunPCA(dkd)
 ElbowPlot(dkd)
@@ -34,75 +36,50 @@ ElbowPlot(dkd)
 dkd <- FindNeighbors(dkd, reduction = "pca", dims = 1:8)
 dkd <- FindClusters(dkd, resolution = 0.38) 
 dkd <- RunUMAP(dkd, reduction = "pca", dims = 1:8)
+dkd <- RunTSNE(dkd, reduction = "pca", dims = 1:8)
 
-library(ggsci)
-p1 <- DimPlot(dkd, 
-                    reduction = "umap", 
-                    group.by = "seurat_clusters",
-                    label = T,          
-                    label.size = 5,         
-                    label.box = TRUE,       
-                    repel = TRUE,           
-                    pt.size = 0.05) +       
-  scale_color_npg() +                      
-  theme(legend.position = "none") +       
-  ggtitle("Cell Clusters (UMAP)") +         
-  NoAxes()                                
-
+p1 <- sc_dim(dkd, reduction = "tsne", geom = geom_bgpoint, pointsize = 1)
 p1
+
 
 
 ## Findmarkers
 markers <- FindAllMarkers(dkd, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.5)
 d <- markers %>%
   group_by(cluster) %>%
-  slice_max(n = 5, order_by = avg_log2FC)
+  slice_max(n = 15, order_by = avg_log2FC)
 
-print(d, n=100) 
+print(d, n = 240) 
 
 ## cell annotation
-new_cluster_ids <- c(
-  "NK/T Cells",             # 0
-  "Proximal Tubule",        # 1
-  "Proximal Tubule",        # 2
-  "LOH/Distal Tubule",      # 3
-  "Injured Tubule",         # 4
-  "Endothelial Cells",      # 5
-  "LOH (Thin Limb)",        # 6
-  "Mesangial/Fibroblasts",  # 7
-  "B Cells",                # 8
-  "Macrophages",            # 9
-  "Endothelial Cells",      # 10
-  "Neutrophils",            # 11
-  "Collecting Duct-PC",     # 12
-  "Collecting Duct-IC",     # 13
-  "NK/T Cells",             # 14
-  "Fibroblasts/Pericytes"   # 15
+# marker
+new.cluster.ids <- c(
+  "T_NK",             # Cluster 0
+  "Proximal tubule",  # Cluster 1
+  "Proximal tubule",  # Cluster 2
+  "DCT-LOH",          # Cluster 3 (SLC12A1)
+  "DCT",              # Cluster 4 (KRT19/MMP7)
+  "EC",               # Cluster 5
+  "LOH",              # Cluster 6 (CLDN14)
+  "Fibroblast",       # Cluster 7 (RGS5/TAGLN)
+  "B cells",          # Cluster 8
+  "Mono",             # Cluster 9 (CD1C/LYZ)
+  "EC",               # Cluster 10
+  "Neutrophils",      # Cluster 11
+  "CD-PC",            # Cluster 12
+  "CD-IC",            # Cluster 13
+  "T cells",             # Cluster 14
+  "Fibroblast"        # Cluster 15
 )
 
-Idents(dkd) <- "seurat_clusters"
 
-names(new_cluster_ids) <- levels(dkd)
-dkd <- RenameIdents(dkd, new_cluster_ids)
-
+names(new.cluster.ids) <- levels(dkd)
+dkd <- RenameIdents(dkd, new.cluster.ids)
 dkd$cell_type <- Idents(dkd)
 
-DimPlot(dkd, reduction = "umap", group.by = "cell_type", label = TRUE, label.size = 4, repel = TRUE) +
-  ggtitle("Annotated Cell Types") +
-  NoAxes()
-
-p2 <- DimPlot(dkd, 
-              reduction = "umap", 
-              group.by = "cell_type",
-              label = T,          
-              label.size = 4,         
-              label.box = TRUE,       
-              repel = TRUE,           
-              pt.size = 0.05) +       
-  scale_color_npg() +                      
-  theme(legend.position = "none") +       
-  ggtitle("Cell Clusters (UMAP)") +         
-  NoAxes()   
+## tsne after annotation
+p2 <- sc_dim(dkd, reduction = "tsne", geom = geom_bgpoint, pointsize = 1) + theme_classic()
 p2
 
 
+saveRDS(dkd, file = "../data/dkd_sce_final.rds")
